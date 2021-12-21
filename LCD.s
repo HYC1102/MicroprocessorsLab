7cfecc85396b01544916a07c41ff362fa757ceec
thevlper <70048876+thevlper@users.noreply.github.com>
2021-12-21 12:38:49 +0000

    Touchscreen_piano with recording

--- LCD.s
+++ LCD.s
@@ -1,72 +1,58 @@
 #include <xc.inc>
 
-global  LCD_Setup, LCD_Write_Message, LCD_Write_Hex
+global  LCD_Setup, LCD_Write_Message
 
 psect	udata_acs   ; named variables in access ram
-LCD_cnt_l:	ds 1	; reserve 1 byte for variable LCD_cnt_l
-LCD_cnt_h:	ds 1	; reserve 1 byte for variable LCD_cnt_h
-LCD_cnt_ms:	ds 1	; reserve 1 byte for ms counter
-LCD_tmp:	ds 1	; reserve 1 byte for temporary use
-LCD_counter:	ds 1	; reserve 1 byte for counting through nessage
+LCD_cnt_l:	ds 1   ; reserve 1 byte for variable LCD_cnt_l
+LCD_cnt_h:	ds 1   ; reserve 1 byte for variable LCD_cnt_h
+LCD_cnt_ms:	ds 1   ; reserve 1 byte for ms counter
+LCD_nternter:	ds 1   ; reserve 1 byte for counting through nessage
+LCD_counter:	ds 1
 
-PSECT	udata_acs_ovr,space=1,ovrld,class=COMRAM
-LCD_hex_tmp:	ds 1    ; reserve 1 byte for variable LCD_hex_tmp
-
-	LCD_E	EQU 5	; LCD enable bit
-    	LCD_RS	EQU 4	; LCD register select bit
-
+	LCD_CS1	EQU 0
+	LCD_CS2	EQU 1
+    	LCD_RS	EQU 2	; LCD register select bit
+	LCD_RW	EQU 3	; LCD Read / Write 
+	LCD_E	EQU 4	; LCD enable bit
+	LCD_RST	EQU 5	; LCD Reset
+	
 psect	lcd_code,class=CODE
     
 LCD_Setup:
 	clrf    LATB, A
-	movlw   11000000B	    ; RB0:5 all outputs
+	movlw   11000000B	    ; RB0:5 all outputs (control line)
 	movwf	TRISB, A
+	clrf    LATD, A
+	movlw   0x00		    ; RD0:8 all outputs (data line)
+	movwf	TRISD, A
+	bcf	LATB, LCD_CS1, A    ;Select both chip 1 & 2
+	bcf	LATB, LCD_CS2, A
+	bsf	LATB, LCD_RST, A
+	bcf	LATB, LCD_RW, A
+	bcf     LATB, LCD_RS, A
+	bcf	LATB, LCD_E, A
 	movlw   40
-	call	LCD_delay_ms	; wait 40ms for LCD to start up properly
-	movlw	00110000B	; Function set 4-bit
-	call	LCD_Send_Byte_I
-	movlw	10		; wait 40us
-	call	LCD_delay_x4us
-	movlw	00101000B	; 2 line display 5x8 dot characters
-	call	LCD_Send_Byte_I
-	movlw	10		; wait 40us
-	call	LCD_delay_x4us
-	movlw	00101000B	; repeat, 2 line display 5x8 dot characters
-	call	LCD_Send_Byte_I
-	movlw	10		; wait 40us
-	call	LCD_delay_x4us
-	movlw	00001111B	; display on, cursor on, blinking on
-	call	LCD_Send_Byte_I
-	movlw	10		; wait 40us
-	call	LCD_delay_x4us
-	movlw	00000001B	; display clear
-	call	LCD_Send_Byte_I
-	movlw	2		; wait 2ms
-	call	LCD_delay_ms
-	movlw	00000110B	; entry mode incr by 1 no shift
-	call	LCD_Send_Byte_I
+	call	LCD_delay_ms	    ; wait 40ms for LCD to start up properly
+	movlw	0x3E		    ; Display Off
+	call	LCD_Send_Instruction
+	movlw	0x40		    ; Set X address (Page 0)
+	call	LCD_Send_Instruction
+	movlw	0xB8		    ; Set Y address (Column 0)
+	call	LCD_Send_Instruction
+	movlw	0x3F		    ; Display On
+	call	LCD_Send_Instruction
+	return
+
+LCD_Send_Instruction:
+    	call	LCD_Send_Byte_I
 	movlw	10		; wait 40us
 	call	LCD_delay_x4us
 	return
-
-LCD_Write_Hex:			; Writes byte stored in W as hex
-	movwf	LCD_hex_tmp, A
-	swapf	LCD_hex_tmp, W, A	; high nibble first
-	call	LCD_Hex_Nib
-	movf	LCD_hex_tmp, W, A	; then low nibble
-LCD_Hex_Nib:			; writes low nibble as hex character
-	andlw	0x0F
-	movwf	LCD_tmp, A
-	movlw	0x0A
-	cpfslt	LCD_tmp, A
-	addlw	0x07		; number is greater than 9 
-	addlw	0x26
-	addwf	LCD_tmp, W, A
-	call	LCD_Send_Byte_D ; write out ascii
-	return	
 	
-LCD_Write_Message:	    ; Message stored at FSR2, length stored in W
+LCD_Write_Message:		    ; Message stored at FSR2, length stored in W
+	movlw	64
 	movwf   LCD_counter, A
+	
 LCD_Loop_message:
 	movf    POSTINC2, W, A
 	call    LCD_Send_Byte_D
@@ -74,33 +60,17 @@ LCD_Loop_message:
 	bra	LCD_Loop_message
 	return
 
-LCD_Send_Byte_I:	    ; Transmits byte stored in W to instruction reg
-	movwf   LCD_tmp, A
-	swapf   LCD_tmp, W, A   ; swap nibbles, high nibble goes first
-	andlw   0x0f	    ; select just low nibble
-	movwf   LATB, A	    ; output data bits to LCD
+LCD_Send_Byte_I:		; Transmits byte stored in W 
+	movwf   LATD, A		; output data bits to LCD
 	bcf	LATB, LCD_RS, A	; Instruction write clear RS bit
-	call    LCD_Enable  ; Pulse enable Bit 
-	movf	LCD_tmp, W, A   ; swap nibbles, now do low nibble
-	andlw   0x0f	    ; select just low nibble
-	movwf   LATB, A	    ; output data bits to LCD
-	bcf	LATB, LCD_RS, A	; Instruction write clear RS bit
-        call    LCD_Enable  ; Pulse enable Bit 
+	call    LCD_Enable	; Pulse enable Bit 
 	return
 
-LCD_Send_Byte_D:	    ; Transmits byte stored in W to data reg
-	movwf   LCD_tmp, A
-	swapf   LCD_tmp, W, A	; swap nibbles, high nibble goes first
-	andlw   0x0f	    ; select just low nibble
-	movwf   LATB, A	    ; output data bits to LCD
-	bsf	LATB, LCD_RS, A	; Data write set RS bit
-	call    LCD_Enable  ; Pulse enable Bit 
-	movf	LCD_tmp, W, A	; swap nibbles, now do low nibble
-	andlw   0x0f	    ; select just low nibble
-	movwf   LATB, A	    ; output data bits to LCD
-	bsf	LATB, LCD_RS, A	; Data write set RS bit	    
-        call    LCD_Enable  ; Pulse enable Bit 
-	movlw	10	    ; delay 40us
+LCD_Send_Byte_D:		    ; Transmits byte stored in W
+	movwf   LATD	, A	    ; output data bits to LCD
+	bsf	LATB, LCD_RS, A	    ; Data write set RS bit
+	call    LCD_Enable	    ; Pulse enable Bit 
+	movlw	10		    ; delay 40us
 	call	LCD_delay_x4us
 	return
 
@@ -123,8 +93,7 @@ LCD_Enable:	    ; pulse enable bit LCD_E for 500ns
 	nop
 	bcf	LATB, LCD_E, A	    ; Writes data to LCD
 	return
-    
-; ** a few delay routines below here as LCD timing can be quite critical ****
+   
 LCD_delay_ms:		    ; delay given in ms in W
 	movwf	LCD_cnt_ms, A
 lcdlp2:	movlw	250	    ; 1 ms delay
@@ -152,4 +121,4 @@ lcdlp1:	decf 	LCD_cnt_l, F, A	; no carry when 0x00 -> 0xff
 	return			; carry reset so return
 
 
-end
+    end
--- main.s
+++ main.s
@@ -1,71 +1,84 @@
 #include <xc.inc>
+Global	Recording, Replay
 
-extrn	UART_Setup, UART_Transmit_Message  ; external uart subroutines
-extrn	LCD_Setup, LCD_Write_Message, LCD_Write_Hex ; external LCD subroutines
-extrn	ADC_Setup, ADC_Read		   ; external ADC subroutines
+extrn	LCD_Setup
+extrn	Init_piano
+extrn	ADC_Setup, Change_Freq
+extrn	DAC_Setup, DAC_Int_Hi, Clear_Recording, freq_replay
 	
 psect	udata_acs   ; reserve data space in access ram
-counter:    ds 1    ; reserve one byte for a counter variable
-delay_count:ds 1    ; reserve one byte for counter in the delay routine
-    
-psect	udata_bank4 ; reserve data anywhere in RAM (here at 0x400)
-myArray:    ds 0x80 ; reserve 128 bytes for message data
-
-psect	data    
-	; ******* myTable, data in programme memory, and its length *****
-myTable:
-	db	'H','e','l','l','o',' ','W','o','r','l','d','!',0x0a
-					; message, plus carriage return
-	myTable_l   EQU	13	; length of data
-	align	2
-    
+delay_count: ds 1    ; reserve one byte for counter in the delay routine
+freq_rollover: ds 1
+delay_num:  ds 1
+Recording:  ds 1
+Replay: ds 1
+    	
 psect	code, abs	
 rst: 	org 0x0
  	goto	setup
 
-	; ******* Programme FLASH read Setup Code ***********************
-setup:	bcf	CFGS	; point to Flash program memory  
-	bsf	EEPGD 	; access Flash program memory
-	call	UART_Setup	; setup UART
-	call	LCD_Setup	; setup UART
-	call	ADC_Setup	; setup ADC
+int_hi:	
+	org	0x0008	; high priority interrpy
+	movf	freq_rollover, W, A	; Move freq_rollover to W
+	btfsc	Replay, 0, A
+	movf	freq_replay, W, A	; Move replay to W if replay mode enabled
+	goto	DAC_Int_Hi
+	; * Programme FLASH read Setup Code *
+setup:	
+	bcf	CFGS		; point to Flash program memory  
+	bsf	EEPGD		; access Flash program memory
+	call	LCD_Setup	
+	call	Init_piano
+	call	ADC_Setup
+	call	DAC_Setup
+	setf	TRISC, A    ; all input (control unit for start & stop replay / recording and clear memory) 
 	goto	start
 	
-	; ******* Main programme ****************************************
-start: 	lfsr	0, myArray	; Load FSR0 with address in RAM	
-	movlw	low highword(myTable)	; address of data in PM
-	movwf	TBLPTRU, A		; load upper bits to TBLPTRU
-	movlw	high(myTable)	; address of data in PM
-	movwf	TBLPTRH, A		; load high byte to TBLPTRH
-	movlw	low(myTable)	; address of data in PM
-	movwf	TBLPTRL, A		; load low byte to TBLPTRL
-	movlw	myTable_l	; bytes to read
-	movwf 	counter, A		; our counter register
-loop: 	tblrd*+			; one byte from PM to TABLAT, increment TBLPRT
-	movff	TABLAT, POSTINC0; move data from TABLAT to (FSR0), inc FSR0	
-	decfsz	counter, A		; count down to zero
-	bra	loop		; keep going until finished
-		
-	movlw	myTable_l	; output message to UART
-	lfsr	2, myArray
-	call	UART_Transmit_Message
+	; * Main programme **
+start: 	
+	btfss	Replay, 0, A
+	call	Change_Freq		; Stores detected input to W
+	movwf	freq_rollover, A	; Move W to freq-rollover
+	
+	btfsc	PORTC, 0, A      ; If Pin 0 of Port C pressed, set Recording True
+	bsf	Recording, 0, A
+	
+	btfsc	PORTC, 1, A	  ; If Pin 1 of Port C pressed, set Recording False
+	bcf	Recording, 0, A
+	
+	btfsc	PORTC, 2, A      ; If Pin 2 of Port C pressed, Clear Recording
+	call	Clear_Recording
+	
+	btfsc	PORTC, 3, A      ; If Pin 3 of Port C pressed, Set replay True
+	call	Replay_Music
+	
+	btfsc	PORTC, 4, A      ; If Pin 4 of Port C pressed, Set Replay False
+	bcf	Replay, 0, A
+	
 
-	movlw	myTable_l-1	; output message to LCD
-				; don't send the final carriage return to LCD
-	lfsr	2, myArray
-	call	LCD_Write_Message
+	movlw	0xAA		; Add in some delay to reduce boucing issues
+	movwf	delay_num, A
+
+loop:
+	movlw	0xFF
+	movwf	delay_count, A
+	call	delay
+	decfsz	delay_num, A
+	bra	loop
 	
-measure_loop:
-	call	ADC_Read
-	movf	ADRESH, W, A
-	call	LCD_Write_Hex
-	movf	ADRESL, W, A
-	call	LCD_Write_Hex
-	goto	measure_loop		; goto current line in code
+	bra	start
 	
+	goto	$		; goto current line in code
+
 	; a delay subroutine if you need one, times around loop in delay_count
 delay:	decfsz	delay_count, A	; decrement until zero
 	bra	delay
 	return
 
+Replay_Music:	; Called when RC3 pressed
+	bsf	Replay, 0, A
+	lfsr	0, 0x200        ; Start memory storage at Bank 2, use FSR0
+
+	return
+	
 	end	rst
